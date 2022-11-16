49802482dae1ee67aed1094ca1a5007916cc973e
Zsombor Joel Gyurkovics <zsomborjoel@gmail.com>
2022-11-16 22:41:01 +0100

    Setup tailwind with vite

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import tailwindConfig from './tailwind.config.cjs';
+
+export default {
+    plugins: [tailwind(tailwindConfig), autoprefixer],
+};
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -2,6 +2,6 @@ import React, { FC } from 'react';
 
 export type Props = {};
 
-const HomePage: FC<Props> = () => <div>Welcome!</div>;
+const HomePage: FC<Props> = () => <h1 className="bg-black text-3xl font-bold underline">Hello world!</h1>;
 
 export default HomePage;
